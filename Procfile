efb59497aabf05ca3044fac67303187c4f7635a0
Roman Parashchenko <romikoops1@gmail.com>
2013-09-13 03:34:23 +0300

    init version

---
+++ Procfile
@@ -0,0 +1,3 @@
+worker1: ruby oil.rb
+#worker2: ruby eur_jpy.rb
+#worker3: ruby gbp_aud.rb
---
+++ worker.rb
@@ -0,0 +1,20 @@
+require 'eventmachine'
+require 'json'
+file_name = "#{ASSET_LABEL.gsub('/', '_').gsub(' ', '_')}.txt"
+File.open(file_name, 'w+')
+
+result = []
+EM.run do
+  EM.add_periodic_timer(1) do
+    EM.defer do
+      price = %x[#{CURL_REQ} -s].split(',')[29]
+      result << [Time.now.utc.to_i, price].to_json
+      if result.size == 60
+        tmp = result
+        result = []
+        File.open(file_name, 'a+'){|f| f.puts tmp}
+      end
+    end
+  end
+end
+
